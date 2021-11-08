ac1c2b2bd6cf5f0c8d39441bc28b3abd733d3e6d
Jorge Jimenez <georgeguitarra07@gmail.com>
2021-11-08 23:43:49 +0100

    INIT graphql server example

--- server.js
+++ server.js
@@ -1,8 +1,27 @@
 const express = require("express");
 const app = express();
 
+// Graphql imports
+const { graphqlHTTP } = require("express-graphql");
+const schema = require("./schema");
+
 app.get("/", (req, res) => {
   res.send("Hello World");
 });
+
+// Root Resolver for Graphql
+const root = { hola: () => "Hola mundo desde Graphql" };
+
+app.use(
+  "/graphql",
+  graphqlHTTP({
+    // Parsin schema
+    schema,
+    // Resolver as root value
+    rootValue: root,
+    //  use Graphiql
+    graphiql: true,
+  })
+);
 
 app.listen(5000, () => console.log("Server running on http://localhost:5000"));
