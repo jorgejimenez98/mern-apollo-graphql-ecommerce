2c91a0a7e2309a8208e110ea6a9ddb99e3fa69e9
Jorge Jimenez <georgeguitarra07@gmail.com>
2021-11-09 00:16:38 +0100

    Create Client Input example

--- schema.js
+++ schema.js
@@ -6,15 +6,24 @@ const schema = buildSchema(`
         name: String
         lastname: String
         company: String
-        emails: [Email]
-    }
-
-    type Email {
         email: String
     }
 
+
     type Query {
         client: Client
+    }
+
+    input ClientInput {
+        id: ID
+        name: String!
+        lastname: String!
+        company: String!
+        email: String! 
+    }
+
+    type Mutation {
+        createClient(input: ClientInput): Client
     }
 `);
 
