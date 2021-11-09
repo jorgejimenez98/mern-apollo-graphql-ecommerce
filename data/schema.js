d3fe0a0d56443288d15ba3dc1df59fd0726b35a4
Jorge Jimenez <georgeguitarra07@gmail.com>
2021-11-09 03:57:52 +0100

    Add import only typeDefs

--- data/schema.js
+++ data/schema.js
@@ -1,8 +1,5 @@
-const resolvers = require("../resolvers");
 const { importSchema } = require("graphql-import");
-import { makeExecutableSchema } from "@graphql-tools/schema";
 
 const typeDefs = importSchema("data/schema.graphql");
-const schema = makeExecutableSchema({ typeDefs, resolvers });
 
-module.exports = schema;
+module.exports = typeDefs;
