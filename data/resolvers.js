396214fa2bce78b6bc3a46719c9bfecb95c6b7e7
Jorge Jimenez <georgeguitarra07@gmail.com>
2021-11-09 01:59:41 +0100

    Update: createClient mutation on mongoose conection

--- data/resolvers.js
+++ data/resolvers.js
@@ -1,16 +1,10 @@
+const { rejects } = require("assert");
+const { resolve } = require("path");
+
+// Models
+const Client = require("../model/client.model");
+
 // Root Resolver for Graphql
-class Client {
-  constructor(id, { name, lastname, company, emails, age, type, orders }) {
-    this.id = id;
-    this.name = name;
-    this.lastname = lastname;
-    this.company = company;
-    this.emails = emails;
-    this.age = age;
-    this.type = type;
-    this.orders = orders;
-  }
-}
 
 const clientsDB = [];
 
@@ -21,11 +15,32 @@ const resolvers = {
     },
   },
   Mutation: {
-    createClient: ({ input }) => {
-      const id = require("crypto").randomBytes(10).toString("hex");
-      clientsDB[id] = input;
-      return new Client(id, input);
+    // Create Client Mutation
+    createClient: (root, { input }) => {
+      // Create Client Model
+      const newClient = new Client({
+        name: input.name,
+        lastname: input.lastname,
+        company: input.company,
+        emails: input.emails,
+        age: input.age,
+        type: input.type,
+        orders: input.orders,
+      });
+      // Set ID
+      newClient.id = newClient._id;
+
+      // Return Promise
+      return new Promise((resolve, object) => {
+        newClient.save((error) => {
+          // Check Error
+          if (error) rejects(error);
+          // Return new Client
+          else resolve(newClient);
+        });
+      });
     },
+    // New Mutation Goes Here
   },
 };
 
