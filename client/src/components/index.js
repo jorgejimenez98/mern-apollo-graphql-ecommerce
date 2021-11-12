56f5492ccefdf1e2e5347c596f4cf699c4b508d3
Jorge Jimenez <georgeguitarra07@gmail.com>
2021-11-12 02:10:22 +0100

    Add loader and message to index imports

--- client/src/components/index.js
+++ client/src/components/index.js
@@ -1,4 +1,6 @@
 import ScrollTop from "./ScrollTop";
 import Header from "./Header";
+import Loader from "./Loader";
+import Message from "./Message";
 
-export { ScrollTop, Header };
+export { ScrollTop, Header, Loader, Message };
