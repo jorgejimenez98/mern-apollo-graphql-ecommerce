a8b03a4aa3c6be06820b3a64aeeb3dbbf2cd9abb
Jorge Jimenez <georgeguitarra07@gmail.com>
2021-11-12 02:25:08 +0100

    Add page403 component to import

--- client/src/components/index.js
+++ client/src/components/index.js
@@ -2,5 +2,6 @@ import ScrollTop from "./ScrollTop";
 import Header from "./Header";
 import Loader from "./Loader";
 import Message from "./Message";
+import Page403 from "./Page403";
 
-export { ScrollTop, Header, Loader, Message };
+export { ScrollTop, Header, Loader, Message, Page403 };
