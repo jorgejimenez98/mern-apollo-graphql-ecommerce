e17be50acf9421005e400de2b5f3abd7937b1c7a
Jorge Jimenez <georgeguitarra07@gmail.com>
2021-11-12 03:19:53 +0100

    ADD table to component

--- client/src/screens/Clients/List.jsx
+++ client/src/screens/Clients/List.jsx
@@ -1,23 +1,40 @@
 import React from "react";
 import { Query } from "react-apollo";
+import MUIDataTable from "mui-datatables";
 import { CLIENTS_QUERY } from "../../graphql/queries";
 import { Loader, Message } from "../../components";
+import { clientColumns, clientlistOptions } from "../../core/mui-datatable";
 
-const ClientsList = () => (
-  <Query query={CLIENTS_QUERY}>
-    {/* Show Consult States */}
-    {({ loading, error, data }) => (
-      <React.Fragment>
-        {loading ? (
-          <Loader />
-        ) : error ? (
-          <Message text={error.message} type="error" />
-        ) : (
-          <React.Fragment>All is OK</React.Fragment>
-        )}
-      </React.Fragment>
-    )}
-  </Query>
-);
+const ClientsList = () => {
+  return (
+    <Query query={CLIENTS_QUERY}>
+      {/* Show Consult States */}
+      {({ loading, error, data }) => {
+        return (
+          <React.Fragment>
+            {loading ? (
+              <Loader />
+            ) : error ? (
+              <Message text={error.message} type="error" />
+            ) : (
+              data?.getClientsList && (
+                <React.Fragment>
+                  <div className="container mt-3">
+                    <MUIDataTable
+                      title={`Clients List (${data.getClientsList.length})`}
+                      data={data.getClientsList}
+                      columns={clientColumns}
+                      options={clientlistOptions}
+                    />
+                  </div>
+                </React.Fragment>
+              )
+            )}
+          </React.Fragment>
+        );
+      }}
+    </Query>
+  );
+};
 
 export default ClientsList;
