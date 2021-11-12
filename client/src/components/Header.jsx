b62eafdda6438e0061c7bf20154eab6cfd23cf3e
Jorge Jimenez <georgeguitarra07@gmail.com>
2021-11-12 01:41:25 +0100

    Change menu labels

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,11 +1,11 @@
-import * as React from 'react';
-import AppBar from '@mui/material/AppBar';
-import Box from '@mui/material/Box';
-import Toolbar from '@mui/material/Toolbar';
-import Typography from '@mui/material/Typography';
-import Button from '@mui/material/Button';
-import IconButton from '@mui/material/IconButton';
-import MenuIcon from '@mui/icons-material/Menu';
+import * as React from "react";
+import AppBar from "@mui/material/AppBar";
+import Box from "@mui/material/Box";
+import Toolbar from "@mui/material/Toolbar";
+import Typography from "@mui/material/Typography";
+import Button from "@mui/material/Button";
+import IconButton from "@mui/material/IconButton";
+import MenuIcon from "@mui/icons-material/Menu";
 
 export default function Header() {
   return (
@@ -22,9 +22,12 @@ export default function Header() {
             <MenuIcon />
           </IconButton>
           <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
-            News
+            GraphQL Ecommerce
           </Typography>
-          <Button color="inherit">Login</Button>
+
+          <Button color="inherit" variant="outlined">
+            New Client
+          </Button>
         </Toolbar>
       </AppBar>
     </Box>
