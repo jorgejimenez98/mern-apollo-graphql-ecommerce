749751a52b486eebebf5105fba9b22b8d81770e8
Jorge Jimenez <georgeguitarra07@gmail.com>
2021-11-12 02:26:02 +0100

    Create TheLayout component

---
+++ client/src/components/TheLayout.jsx
@@ -0,0 +1,39 @@
+import React from "react";
+import { Header, ScrollTop } from "./index";
+import { routes } from "../routes";
+import {
+  BrowserRouter as Router,
+  Route,
+  Redirect,
+  Switch,
+} from "react-router-dom";
+
+function TheLayout() {
+  return (
+    <Router>
+      <div>
+        <Header />
+        <ScrollTop />
+        <Switch>
+          {routes.map((route, idx) => {
+            return (
+              route.component && (
+                <Route
+                  key={idx}
+                  path={route.path}
+                  exact={route.exact}
+                  render={(props) => <route.component {...props} />}
+                />
+              )
+            );
+          })}
+          <Redirect from="/" to="/clients/list" />
+        </Switch>
+        <Footer />
+        <ScrollTop />
+      </div>
+    </Router>
+  );
+}
+
+export default TheLayout;
