743120bc016e20e6023d1175a3a30039572b0015
Jorge Jimenez <georgeguitarra07@gmail.com>
2021-11-12 02:41:29 +0100

    Create clients list columns

---
+++ client/src/core/mui-datatable/clients-list/columns.js
@@ -0,0 +1,81 @@
+import Tooltip from "@mui/material/Tooltip";
+import IconButton from "@mui/material/IconButton";
+import EditIcon from "@mui/icons-material/Edit";
+
+export const clientColumns = [
+  {
+    name: "id",
+    label: "",
+    options: {
+      filter: false,
+      sort: false,
+      display: false,
+      viewColumns: false,
+    },
+  },
+  {
+    name: "name",
+    label: "Full Name",
+    options: {
+      filter: true,
+      sort: true,
+      viewColumns: true,
+      customBodyRender: (value, tableMeta) => {
+        const name = value;
+        const lastName = tableMeta.rowData[4];
+        return (
+          <p>
+            <strong>
+              {name} {lastName}
+            </strong>
+          </p>
+        );
+      },
+    },
+  },
+  {
+    name: "company",
+    label: "Company",
+    options: {
+      filter: false,
+      sort: false,
+      viewColumns: false,
+      customBodyRender: (company) => {
+        return <p>{company}</p>;
+      },
+    },
+  },
+  {
+    name: "id",
+    label: "Acciones",
+    options: {
+      filter: false,
+      viewColumns: false,
+      sort: false,
+      print: false,
+      download: false,
+      customBodyRender: (clientId, tableMeta) => {
+        const name = tableMeta.rowData[1];
+        return (
+          <LinkContainer to={`/client/edit/${clientId}`}>
+            <Tooltip title={`Edit Client: ${name}`} placement="bottom">
+              <IconButton>
+                <EditIcon />
+              </IconButton>
+            </Tooltip>
+          </LinkContainer>
+        );
+      },
+    },
+  },
+  {
+    name: "lastname",
+    label: "lastname",
+    options: {
+      filter: false,
+      sort: false,
+      display: false,
+      viewColumns: false,
+    },
+  },
+];
