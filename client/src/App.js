8bce1d0d2df1df6c2aa219fd8ff4b92b8caed274
Jorge Jimenez <georgeguitarra07@gmail.com>
2021-11-12 01:19:32 +0100

    Init Apollo client on main component

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,24 @@
 import React from "react";
+// Apollo Deps
+import { ApolloProvider } from "react-apollo";
+import ApolloClient from "apollo-boost";
+
+// Apollo Client
+const client = new ApolloClient({
+  uri: "http://localhost:5000/graphql",
+  onError: ({ netWorkError, graphqlError }) => {
+    console.log("Network Error", netWorkError);
+    console.log("GraphQL Error", graphqlError);
+  },
+});
 
 function App() {
   return (
-    <div>
-      <h1>Hola</h1>
-    </div>
+    /* Init Apollo Provider */
+    <ApolloProvider client={client}>
+      {/* Components APP goes here */}
+      <h1>Hello World</h1>
+    </ApolloProvider>
   );
 }
 
