a6d8503cb0d1d77f1465d7993293581e762c48f5
Jorge Jimenez <georgeguitarra07@gmail.com>
2021-11-12 02:10:45 +0100

    Init consult list clients

--- client/src/screens/Clients/List.jsx
+++ client/src/screens/Clients/List.jsx
@@ -1,7 +1,23 @@
 import React from "react";
+import { Query } from "react-apollo";
+import { CLIENTS_QUERY } from "../../graphql/queries";
+import { Loader, Message } from "../../components";
 
-function ClientsList() {
-  return <div>List</div>;
-}
+const ClientsList = () => (
+  <Query query={CLIENTS_QUERY}>
+    {/* Show Consult States */}
+    {({ loading, error, data }) => (
+      <React.Fragment>
+        {loading ? (
+          <Loader />
+        ) : error ? (
+          <Message text={error.message} type="error" />
+        ) : (
+          <React.Fragment>All is OK</React.Fragment>
+        )}
+      </React.Fragment>
+    )}
+  </Query>
+);
 
 export default ClientsList;
