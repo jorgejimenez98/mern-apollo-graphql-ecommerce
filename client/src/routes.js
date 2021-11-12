f9bda2de3c762156d49d02123d130ae04733cca7
Jorge Jimenez <georgeguitarra07@gmail.com>
2021-11-12 02:25:22 +0100

    Create app routes, add client list component

---
+++ client/src/routes.js
@@ -0,0 +1,9 @@
+// Components
+import { ClientsList } from "./screens";
+
+const routes = [
+  // Routes
+  { path: "/clients/list", exact: true, component: ClientsList },
+];
+
+export default routes;
