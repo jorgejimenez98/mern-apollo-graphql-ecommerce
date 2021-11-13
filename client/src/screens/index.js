9af52dabf93c5e336ae4da22f892ea29006acd72
Jorge Jimenez <georgeguitarra07@gmail.com>
2021-11-14 00:58:46 +0100

    ADD add client component to index

--- client/src/screens/index.js
+++ client/src/screens/index.js
@@ -1,6 +1,7 @@
 import ClientsList from "./Clients/List";
 import Login from "./Login/Login";
 import Edit from "./Clients/Edit";
+import ADD from "./Clients/Add";
 import ClientOrders from "./Orders/ClientOrders/ClientOrders";
 
-export { ClientsList, Login, Edit, ClientOrders };
+export { ClientsList, Login, Edit, ClientOrders, ADD };
