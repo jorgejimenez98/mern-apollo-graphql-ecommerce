0944611f6ce5d960bcdd85b78647e2742f1f2b43
Jorge Jimenez <georgeguitarra07@gmail.com>
2021-11-13 22:24:31 +0100

    Change react-router-dom imports, add ReactFragment

--- client/src/components/TheLayout.jsx
+++ client/src/components/TheLayout.jsx
@@ -1,36 +1,29 @@
 import React from "react";
 import { Header, ScrollTop } from "./index";
 import routes from "../routes";
-import {
-  BrowserRouter as Router,
-  Route,
-  Redirect,
-  Switch,
-} from "react-router-dom";
+import { Route, Redirect, Switch } from "react-router-dom";
 
 function TheLayout() {
   return (
-    <Router>
-      <div>
-        <Header />
-        <ScrollTop />
-        <Switch>
-          {routes.map((route, idx) => {
-            return (
-              route.component && (
-                <Route
-                  key={idx}
-                  path={route.path}
-                  exact={route.exact}
-                  render={(props) => <route.component {...props} />}
-                />
-              )
-            );
-          })}
-          <Redirect from="/" to="/clients/list" />
-        </Switch>
-      </div>
-    </Router>
+    <React.Fragment>
+      <Header />
+      <ScrollTop />
+      <Switch>
+        {routes.map((route, idx) => {
+          return (
+            route.component && (
+              <Route
+                key={idx}
+                path={route.path}
+                exact={route.exact}
+                render={(props) => <route.component {...props} />}
+              />
+            )
+          );
+        })}
+        <Redirect from="/" to="/clients/list" />
+      </Switch>
+    </React.Fragment>
   );
 }
 
