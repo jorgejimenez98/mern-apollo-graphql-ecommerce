ebdbd1ba077f3ae6a549098bbb253314b2554f45
Jorge Jimenez <georgeguitarra07@gmail.com>
2021-11-14 00:58:56 +0100

    ADD add client component to main routes

--- client/src/routes.js
+++ client/src/routes.js
@@ -1,9 +1,17 @@
 // Components
-import { ClientsList, Edit as EditClient, ClientOrders } from "./screens";
+import {
+  ClientsList,
+  Edit as EditClient,
+  ADD as AddClient,
+  ClientOrders,
+} from "./screens";
 
 const routes = [
   // Routes
+
+  /* Clients routes */
   { path: "/clients/list", exact: true, component: ClientsList },
+  { path: "/client/add", component: AddClient },
   { path: "/client/edit/:clientId", component: EditClient },
   { path: "/client/orders/:clientId", component: ClientOrders },
 ];
