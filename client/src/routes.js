63076850838a994364afcc400bda7c872fb64e40
Jorge Jimenez <georgeguitarra07@gmail.com>
2021-11-13 23:42:01 +0100

    Add edit and orders clients to main routes

--- client/src/routes.js
+++ client/src/routes.js
@@ -1,9 +1,11 @@
 // Components
-import { ClientsList } from "./screens";
+import { ClientsList, Edit as EditClient, ClientOrders } from "./screens";
 
 const routes = [
   // Routes
   { path: "/clients/list", exact: true, component: ClientsList },
+  { path: "/client/edit/:clientId", component: EditClient },
+  { path: "/client/orders/:clientId", component: ClientOrders },
 ];
 
 export default routes;
