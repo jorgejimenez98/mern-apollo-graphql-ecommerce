3894f4fc4ecbc5d1020cb177d5a890d75e24b75e
Jorge Jimenez <georgeguitarra07@gmail.com>
2021-11-13 23:41:45 +0100

    Add edit and orders clients to imports

--- client/src/screens/index.js
+++ client/src/screens/index.js
@@ -1,4 +1,6 @@
 import ClientsList from "./Clients/List";
 import Login from "./Login/Login";
+import Edit from "./Clients/Edit";
+import ClientOrders from "./Orders/ClientOrders/ClientOrders";
 
-export { ClientsList, Login };
+export { ClientsList, Login, Edit, ClientOrders };
