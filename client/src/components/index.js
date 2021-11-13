b71bef02b3e15174c2361e040b65bbc52fc0eb7e
Jorge Jimenez <georgeguitarra07@gmail.com>
2021-11-13 23:30:24 +0100

    Add confirm dialog ti index import

--- client/src/components/index.js
+++ client/src/components/index.js
@@ -4,5 +4,14 @@ import Loader from "./Loader";
 import Message from "./Message";
 import Page403 from "./Page403";
 import TheLayout from "./TheLayout";
+import ConfirmDialog from "./ConfirmDialog";
 
-export { ScrollTop, Header, Loader, Message, Page403, TheLayout };
+export {
+  ScrollTop,
+  Header,
+  Loader,
+  Message,
+  Page403,
+  TheLayout,
+  ConfirmDialog,
+};
