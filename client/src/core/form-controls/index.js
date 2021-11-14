86248ab8adb12c4d8d7903116bd7ff944405a2a9
Jorge Jimenez <georgeguitarra07@gmail.com>
2021-11-14 02:00:24 +0100

    Create forms client index

---
+++ client/src/core/form-controls/index.js
@@ -0,0 +1,17 @@
+import NameFormControl from "./clients-form-controls/NameFormControl";
+import EmailFormControl from "./clients-form-controls/EmailFormControl";
+import CompanyFormControl from "./clients-form-controls/CompanyFormControl";
+import TypeClientFormControl from "./clients-form-controls/TypeClientFormControl";
+import AgeFormControl from "./clients-form-controls/AgeFormControl";
+import LastNameFormControl from "./clients-form-controls/LastNameFormControl";
+
+const clientForms = {
+  NameFormControl,
+  CompanyFormControl,
+  EmailFormControl,
+  TypeClientFormControl,
+  AgeFormControl,
+  LastNameFormControl,
+};
+
+export { clientForms };
