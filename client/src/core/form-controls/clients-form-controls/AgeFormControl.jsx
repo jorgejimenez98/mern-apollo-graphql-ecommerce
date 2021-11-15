759a345ab21ec0022e2ceb04ef8dc0bf3d729e83
Jorge Jimenez <georgeguitarra07@gmail.com>
2021-11-15 02:40:52 +0100

    Change age form component props

--- client/src/core/form-controls/clients-form-controls/AgeFormControl.jsx
+++ client/src/core/form-controls/clients-form-controls/AgeFormControl.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { Form } from "react-bootstrap";
 
-function AgeFormControl({ formik }) {
+function AgeFormControl({ values, handleChange, touched, errors }) {
   return (
     <Form.Group>
       <Form.Label className="float-left text-capitalize">Age</Form.Label>
@@ -9,14 +9,14 @@ function AgeFormControl({ formik }) {
         type="number"
         placeholder={`Enter Age`}
         name={"age"}
-        value={formik.values.age}
-        onChange={formik.handleChange}
-        isInvalid={formik.touched.age && Boolean(formik.errors.age)}
-        isValid={formik.touched.age && !Boolean(formik.errors.age)}
+        value={values.age}
+        onChange={handleChange}
+        isInvalid={touched.age && Boolean(errors.age)}
+        isValid={touched.age && !Boolean(errors.age)}
       />
-      {formik.touched.age && Boolean(formik.errors.age) && (
+      {touched.age && Boolean(errors.age) && (
         <Form.Control.Feedback type="invalid">
-          <span className="float-left">{formik.errors.age}</span>
+          <span className="float-left">{errors.age}</span>
         </Form.Control.Feedback>
       )}
     </Form.Group>
