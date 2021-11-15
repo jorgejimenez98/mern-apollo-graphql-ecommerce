40fff044f9efe88228c218c9f44329e9d0a0197a
Jorge Jimenez <georgeguitarra07@gmail.com>
2021-11-16 00:24:37 +0100

    Change Query to useQuery apollo hook

--- client/src/screens/Clients/List.jsx
+++ client/src/screens/Clients/List.jsx
@@ -1,36 +1,31 @@
 import React from "react";
-import { Query } from "react-apollo";
+import { useQuery } from "@apollo/client";
 import { CLIENTS_QUERY } from "../../graphql/queries";
 import { Loader, Message } from "../../components";
 import { TableClients } from "../../core/mui-datatable";
 
 const ClientsList = () => {
+  const { loading, error, data } = useQuery(CLIENTS_QUERY);
+
   const confirmDelete = (id) => {
     console.log("Delete", id);
   };
 
   return (
-    <Query query={CLIENTS_QUERY}>
-      {/* Show Consult States */}
-      {({ loading, error, data }) => {
-        return (
-          <React.Fragment>
-            {loading ? (
-              <Loader />
-            ) : error ? (
-              <Message text={error.message} type="error" />
-            ) : (
-              data?.getClientsList && (
-                <TableClients
-                  data={data.getClientsList}
-                  confirmDelete={confirmDelete}
-                />
-              )
-            )}
-          </React.Fragment>
-        );
-      }}
-    </Query>
+    <React.Fragment>
+      {loading ? (
+        <Loader />
+      ) : error ? (
+        <Message text={error.message} type="error" />
+      ) : (
+        data?.getClientsList && (
+          <TableClients
+            data={data.getClientsList}
+            confirmDelete={confirmDelete}
+          />
+        )
+      )}
+    </React.Fragment>
   );
 };
 
