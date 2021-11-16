2e781b0616edc512264baa822a30d4448d00cb78
Jorge Jimenez <georgeguitarra07@gmail.com>
2021-11-16 02:45:09 +0100

    Create btn next

--- client/src/core/mui-datatable/custom.paginator.jsx
+++ client/src/core/mui-datatable/custom.paginator.jsx
@@ -1,21 +1,32 @@
 import React from "react";
 
-function Paginator({ actual, offset }) {
+function Paginator({ actual, offset, totalItems, limit, nextPage, lastPage }) {
   const btnBefore =
     actual > 1 ? (
-      <button className="btn btn-success mr-2">&laquo; Before</button>
+      <button onClick={lastPage} className="btn btn-success mr-2">
+        &laquo; Before
+      </button>
     ) : (
       ""
     );
 
-  const btnBefore =
-    actual > 1 ? (
-      <button className="btn btn-success mr-2">&laquo; Before</button>
+  const pages = Math.ceil(totalItems) / limit;
+
+  const btnNext =
+    actual !== pages ? (
+      <button onClick={nextPage} className="btn btn-success">
+        &raquo; Next
+      </button>
     ) : (
       ""
     );
 
-  return <div className="mt-5 d-flex justify-content-center">{btnBefore}</div>;
+  return (
+    <div className="mt-5 d-flex justify-content-center">
+      {btnBefore}
+      {btnNext}
+    </div>
+  );
 }
 
 export default Paginator;
