2006ef2833c8158e2aa54ca74ff0bf49e2a77c29
Jorge Jimenez <georgeguitarra07@gmail.com>
2021-11-17 02:48:46 +0100

    Init product resolvers

---
+++ resolvers/product.resolvers.js
@@ -0,0 +1,18 @@
+const { rejects } = require("assert");
+const { resolve } = require("path");
+
+// Models
+const Product = require("../model/product.model");
+
+// Client Resolver
+const productResolvers = {
+  // Queries
+  Query: {},
+
+  // Mutations
+  Mutation: {
+  
+  },
+};
+
+module.exports = productResolvers;
