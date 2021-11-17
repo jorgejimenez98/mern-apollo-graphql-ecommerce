dcd68ecf5553773cce74c5239a836acaa83e8227
Jorge Jimenez <georgeguitarra07@gmail.com>
2021-11-17 02:50:09 +0100

    Create, insert product resolver

--- resolvers/product.resolvers.js
+++ resolvers/product.resolvers.js
@@ -11,7 +11,27 @@ const productResolvers = {
 
   // Mutations
   Mutation: {
-  
+    // Create Product Resolver
+    createProduct: (root, { input }) => {
+      // Create Client Model
+      const newProduct = new Product({
+        name: input.name,
+        price: input.price,
+        stock: input.stock,
+      });
+
+      // Return Promise
+      return new Promise((resolve, object) => {
+        newProduct.save((error) => {
+          // Check Error
+          if (error) rejects(error);
+          // Return new Client
+          else resolve(newProduct);
+        });
+      });
+    },
+
+    // Next Mutation Here
   },
 };
 
