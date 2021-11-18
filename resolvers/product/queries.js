7f7927296630826a0bd66a69169a3ac91997aff6
Jorge Jimenez <georgeguitarra07@gmail.com>
2021-11-18 03:03:25 +0100

    Create get product list query

--- resolvers/product/queries.js
+++ resolvers/product/queries.js
@@ -1,3 +1,19 @@
-const productQueries = {};
+import Product from "../../model/product.model";
+
+const productQueries = {
+  // Get Product List
+  getProductList: (root, { limit, offset }) => {
+    return new Promise((resolve, object) => {
+        Product.find({}, (error, data) => {
+        // Check Error
+        if (error) rejects(error);
+        // Return new Client
+        else resolve(data);
+      })
+        .limit(limit)
+        .skip(offset);
+    });
+  },
+};
 
 export default productQueries;
