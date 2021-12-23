10b1b7227b01334d42998fafa8cc3cdb4c3932fa
Jorge Jimenez <georgeguitarra07@gmail.com>
2021-12-23 02:47:27 +0100

    Add Add product screen component to index screens

--- client/src/screens/index.js
+++ client/src/screens/index.js
@@ -3,5 +3,7 @@ import Login from "./Login/Login";
 import Edit from "./Clients/Edit";
 import ADD from "./Clients/Add";
 import ClientOrders from "./Orders/ClientOrders/ClientOrders";
+/* Products */
+import { AddProduct } from "./Products";
 
-export { ClientsList, Login, Edit, ClientOrders, ADD };
+export { ClientsList, Login, Edit, ClientOrders, ADD, AddProduct };
