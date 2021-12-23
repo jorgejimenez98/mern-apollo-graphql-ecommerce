0930f19025387556eb393e489ea9ad4f6ea10644
Jorge Jimenez <georgeguitarra07@gmail.com>
2021-12-23 02:45:31 +0100

    Create Custom text input component

---
+++ client/src/core/form-controls/custom-form-controls/text-input.custom.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+import { useField } from "formik";
+import { Form } from "react-bootstrap";
+
+const CustomTextInputComponent = ({ ...props }) => {
+  const [field, meta] = useField(props);
+
+  return (
+    <div>
+      <Form.Group>
+        <Form.Label className="float-left text-capitalize">
+          {props.label}
+        </Form.Label>
+        <Form.Control {...field} {...props} />
+
+        {Boolean(meta.touched && meta.error) ? (
+          <small className="error-text">{meta.error}</small>
+        ) : null}
+      </Form.Group>
+    </div>
+  );
+};
+
+export default CustomTextInputComponent;
