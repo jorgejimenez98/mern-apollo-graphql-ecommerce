67b3f7f8816d819d322df972fe09da84582ba7e4
Jorge Jimenez <georgeguitarra07@gmail.com>
2021-12-28 22:07:41 +0100

    Add the delete functions

--- client/src/screens/Products/ListProduct/ListProduct.jsx
+++ client/src/screens/Products/ListProduct/ListProduct.jsx
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 import { useQuery, useMutation } from "@apollo/client";
 import { TableProducts } from "../../../core/mui-datatable";
 import { PRODUCT_LIST_QUERY } from "../../../graphql/queries";
+import { DELETE_PRODUCT } from "../../../graphql/mutations";
 import { Loader, Message } from "../../../components";
 
 export default function ListProduct() {
@@ -15,21 +16,18 @@ export default function ListProduct() {
     variables: { limit: limit, offset: offset },
   });
 
-  console.log(data)
-
-  /*   const [
+  const [
     deleteClient,
     { data: dataDelete, error: errorDelete, loading: loadingDelete },
-  ] = useMutation(DELETE_CLIENT); */
+  ] = useMutation(DELETE_PRODUCT);
 
   const confirmDelete = (id) => {
-    console.log("Delete product", id);
-    /* deleteClient({ variables: { deleteClientId: id } }); */
+    deleteClient({ variables: { productId: id } });
   };
 
-/*   if (dataDelete) {
-    console.log("Delete OOKOKOK");
-  } */
+  if (dataDelete) {
+    console.log("Delete Product OOKOKOK");
+  }
 
   return (
     <React.Fragment>
@@ -40,8 +38,8 @@ export default function ListProduct() {
       ) : (
         data?.getProductList && (
           <React.Fragment>
-            {/*     {loadingDelete && <Loader />}
-            {errorDelete && <Message text={error.message} type="error" />} */}
+            {loadingDelete && <Loader />}
+            {errorDelete && <Message text={error.message} type="error" />}
 
             <TableProducts
               data={data.getProductList}
