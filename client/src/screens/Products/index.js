d019195354713c073ddd6786af47f731a84f9c12
Jorge Jimenez <georgeguitarra07@gmail.com>
2021-12-29 00:44:34 +0100

    Add edit product component to product index

--- client/src/screens/Products/index.js
+++ client/src/screens/Products/index.js
@@ -1,4 +1,5 @@
 import AddProduct from "./AddProduct/AddProduct";
 import ListProduct from "./ListProduct/ListProduct";
+import EditProduct from "./EditProduct/EditProduct";
 
-export { AddProduct, ListProduct };
+export { AddProduct, ListProduct, EditProduct };
