a8561efded62573c294e8fb3a3e4ff9cf51a5c84
Jorge Jimenez <georgeguitarra07@gmail.com>
2021-12-28 21:57:35 +0100

    Add products list query to index imports

--- client/src/graphql/queries/index.js
+++ client/src/graphql/queries/index.js
@@ -1,3 +1,4 @@
 import { CLIENTS_QUERY, CLIENT_DETAILS } from "./clientQueries";
+import { PRODUCT_LIST_QUERY } from "./product.queries";
 
-export { CLIENTS_QUERY, CLIENT_DETAILS };
+export { CLIENTS_QUERY, CLIENT_DETAILS, PRODUCT_LIST_QUERY };
