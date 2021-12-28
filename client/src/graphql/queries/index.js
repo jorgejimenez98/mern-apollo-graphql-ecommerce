46a462631819e15b1cd2ea883ee232934957c2b2
Jorge Jimenez <georgeguitarra07@gmail.com>
2021-12-29 00:48:00 +0100

    Add get product details query to query index

--- client/src/graphql/queries/index.js
+++ client/src/graphql/queries/index.js
@@ -1,4 +1,9 @@
 import { CLIENTS_QUERY, CLIENT_DETAILS } from "./clientQueries";
-import { PRODUCT_LIST_QUERY } from "./product.queries";
+import { PRODUCT_LIST_QUERY, GET_PRODUCT_DETAILS } from "./product.queries";
 
-export { CLIENTS_QUERY, CLIENT_DETAILS, PRODUCT_LIST_QUERY };
+export {
+  CLIENTS_QUERY,
+  CLIENT_DETAILS,
+  PRODUCT_LIST_QUERY,
+  GET_PRODUCT_DETAILS,
+};
