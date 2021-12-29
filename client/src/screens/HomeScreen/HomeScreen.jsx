3e062cd74de0b8012a98bba1a9477178b42b1766
Jorge Jimenez <georgeguitarra07@gmail.com>
2021-12-29 01:29:54 +0100

    Create home screen component

---
+++ client/src/screens/HomeScreen/HomeScreen.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function HomeScreen() {
+    return (
+        <div className='container text-center'>
+            Home Screen
+        </div>
+    )
+}
+
+export default HomeScreen
