2b1a7d0adf74c490e0603e3e484681b7dfb0f8e7
Jorge Jimenez <georgeguitarra07@gmail.com>
2022-01-09 02:22:15 +0100

    Add select component to state

--- client/src/screens/Orders/ClientOrders/ProductSelect/ProductSelect.jsx
+++ client/src/screens/Orders/ClientOrders/ProductSelect/ProductSelect.jsx
@@ -1,11 +1,27 @@
-import React from 'react'
+import React, { useState } from "react";
+import Select from "react-select";
 
-function ProductSelect() {
-    return (
-        <div>
-            Select goes here
-        </div>
-    )
+function ProductSelect({ list }) {
+  const [selectedProducts, setSelectedProducts] = useState([]);
+
+  const handleChange = (newProducts) => {
+    setSelectedProducts(newProducts);
+  };
+
+  return (
+    <React.Fragment>
+      <h3 className="text-center">Select Products</h3>
+
+      <Select
+        options={list}
+        isMulti={true}
+        onChange={handleChange}
+        getOptionValue={(item) => item.id}
+        getOptionLabel={(item) => item.name}
+        placeholder={"Select Product"}
+      />
+    </React.Fragment>
+  );
 }
 
-export default ProductSelect
+export default ProductSelect;
