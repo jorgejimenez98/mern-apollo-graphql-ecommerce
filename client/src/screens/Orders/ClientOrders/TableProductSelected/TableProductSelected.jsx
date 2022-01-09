fdec7e61ef441312fd73d13f7535254a20070c47
Jorge Jimenez <georgeguitarra07@gmail.com>
2022-01-09 02:37:00 +0100

    Init selected products table

--- client/src/screens/Orders/ClientOrders/TableProductSelected/TableProductSelected.jsx
+++ client/src/screens/Orders/ClientOrders/TableProductSelected/TableProductSelected.jsx
@@ -1,11 +1,45 @@
-import React from 'react'
+import React from "react";
+import * as mui from "@mui/material";
 
-function TableProductSelected() {
-    return (
-        <div>
-            table goes here
-        </div>
-    )
+function TableProductSelected({ list }) {
+  return (
+    <React.Fragment>
+      {list.length !== 0 && (
+        <React.Fragment>
+          <h3 className="mt-3 text-center text-muted">Resume of Products</h3>
+          <mui.TableContainer component={mui.Paper}>
+            <mui.Table sx={{ minWidth: 650 }} aria-label="simple table">
+              <mui.TableHead>
+                <mui.TableRow>
+                  <mui.TableCell>Product</mui.TableCell>
+                  <mui.TableCell align="right">Price</mui.TableCell>
+                  <mui.TableCell align="right">Stock</mui.TableCell>
+                  <mui.TableCell align="right">Quantity</mui.TableCell>
+                  <mui.TableCell align="right"></mui.TableCell>
+                </mui.TableRow>
+              </mui.TableHead>
+              <mui.TableBody>
+                {list.map((row) => (
+                  <mui.TableRow
+                    key={row.id}
+                    sx={{ "&:last-child td, &:last-child th": { border: 0 } }}
+                  >
+                    <mui.TableCell component="th" scope="row">
+                      {row.name}
+                    </mui.TableCell>
+                    <mui.TableCell align="right">{row.price}</mui.TableCell>
+                    <mui.TableCell align="right">{row.stock}</mui.TableCell>
+                    <mui.TableCell align="right">Quantity</mui.TableCell>
+                    <mui.TableCell align="right">Delete</mui.TableCell>
+                  </mui.TableRow>
+                ))}
+              </mui.TableBody>
+            </mui.Table>
+          </mui.TableContainer>
+        </React.Fragment>
+      )}
+    </React.Fragment>
+  );
 }
 
-export default TableProductSelected
+export default TableProductSelected;
