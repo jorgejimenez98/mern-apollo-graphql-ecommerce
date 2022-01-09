7ec59c9167193569580e927c0b72d532831665eb
Jorge Jimenez <georgeguitarra07@gmail.com>
2022-01-09 02:24:18 +0100

    Create selectedProducts table component

---
+++ client/src/screens/Orders/ClientOrders/TableProductSelected/TableProductSelected.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function TableProductSelected() {
+    return (
+        <div>
+            table goes here
+        </div>
+    )
+}
+
+export default TableProductSelected
